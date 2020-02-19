2dc0b871f449eacc4738a82e555b8c79362aa4ac
Daniel Marin <daniel@DESARROLLO-05.sonartelematics.local>
2020-02-19 10:08:48 -0500

    Add GetPartReviews and GetPartReview endpoint

--- Controllers/PartReviewsController.cs
+++ Controllers/PartReviewsController.cs
@@ -1,7 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using OtroEF.Models;
+
 namespace OtroEF.Controllers
 {
-    public class PartReviewsController
+    [Route("[controller]")]
+    [ApiController]
+    public class PartReviewsController : ControllerBase
     {
-        
+        private readonly OtroEFContext _context;
+
+        public PartReviewsController(OtroEFContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PartReview>> GetPartReviews()
+        {
+            return _context.PartReviews;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<PartReview> GetPartReview(int id)
+        {
+            var partReview = _context.PartReviews.Find(id);
+
+            if (partReview == null)
+            {
+                return NotFound();
+            }
+
+            return partReview;
+        }
     }
 }
