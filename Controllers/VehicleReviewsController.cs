793862503a573d7108118cad22b40f5068e811b7
Daniel Marin <daniel@DESARROLLO-05.sonartelematics.local>
2020-02-19 10:20:36 -0500

    Add GetVehicleReviews and GetVehicleReview endpoint

--- Controllers/VehicleReviewsController.cs
+++ Controllers/VehicleReviewsController.cs
@@ -1,7 +1,36 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using OtroEF.Models;
+
 namespace OtroEF.Controllers
 {
-    public class VehicleReviewsController
+    [Route("[controller]")]
+    [ApiController]
+    public class VehicleReviewsController : ControllerBase
     {
-        
+        private readonly OtroEFContext _context;
+        public VehicleReviewsController(OtroEFContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<VehicleReview>> GetVehicleReviews()
+        {
+            return _context.VehicleReviews;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<VehicleReview> GetVehicleReview(int id)
+        {
+            var vehicleReview = _context.VehicleReviews.Find(id);
+
+            if (vehicleReview == null)
+            {
+                return NotFound();
+            }
+
+            return vehicleReview;
+        }
     }
 }
