19ec82fd6f9ca8698f3841a762fd9a0f0e406fd4
Daniel Marin <daniel@DESARROLLO-05.sonartelematics.local>
2020-02-19 09:32:24 -0500

    Delete comments lines from Program.cs

--- Program.cs
+++ Program.cs
@@ -14,29 +14,7 @@ namespace OtroEF
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-            // var host = CreateHostBuilder(args).Build();
-            // CreateDbIfNotExists(host);
-            // host.Run();
         }
-
-        // private static void CreateDbIfNotExists(IHost host)
-        // {
-        //     using (var scope = host.Services.CreateScope())
-        //     {
-        //         var services = scope.ServiceProvider;
-
-        //         try
-        //         {
-        //             var context = services.GetRequiredService<OtroEFContext>();
-        //             context.Database.EnsureCreated();
-        //         }
-        //         catch (Exception ex)
-        //         {
-        //             var logger = services.GetRequiredService<ILogger<Program>>();
-        //             logger.LogError(ex, "An error occurred creating the DB.");
-        //         }
-        //     }
-        // }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
