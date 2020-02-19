4978d4e6f2e342ff86e22f5fe6e35d4141529e77
Daniel Marin <daniel@DESARROLLO-05.sonartelematics.local>
2020-02-19 10:36:15 -0500

    Add GetVehicleParts and GetVehiclePart endpoint

--- Controllers/VehiclePartsController.cs
+++ Controllers/VehiclePartsController.cs
@@ -1,7 +1,36 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using OtroEF.Models;
+
 namespace OtroEF.Controllers
 {
-    public class VehiclePartsController
+    [Route("[controller]")]
+    [ApiController]
+    public class VehiclePartsController : ControllerBase
     {
+        private OtroEFContext _context;
         
+        public VehiclePartsController(OtroEFContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<VehiclePart>> GetVehicleParts()
+        {
+            return _context.VehicleParts;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<VehiclePart> GetVehiclePart(int id)
+        {
+            var vehiclePart = _context.VehicleParts.Find(id);
+
+            if ( vehiclePart == null ) {
+                return NotFound();
+            }
+
+            return vehiclePart;
+        }
     }
 }
