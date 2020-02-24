3c22bf084aa883cda3a387b394210719c030b21f
Daniel Marin <daniel@DESARROLLO-05.sonartelematics.local>
2020-02-24 10:14:45 -0500

    Comment navigation property

--- Models/PartReview.cs
+++ Models/PartReview.cs
@@ -7,12 +7,13 @@ namespace OtroEF.Models
         public int Id { get; set; }
         public DateTime DateReview { get; set; }
         public string Diagnosis { get; set; }
-        public int VehicleReviewId { get; set; }
-        public int VehiclePartId { get; set; }
-        public int EmployeeId { get; set; }
+        
+        // public int VehicleReviewId { get; set; }
+        // public int VehiclePartId { get; set; }
+        // public int EmployeeId { get; set; }
 
-        public VehicleReview VehicleReview { get; set; }
-        public VehiclePart VehiclePart {get; set; }
-        public Employee Employee { get; set; }
+        // public VehicleReview VehicleReview { get; set; }
+        // public VehiclePart VehiclePart {get; set; }
+        // public Employee Employee { get; set; }
     }
 }
