79dafe31f87fe0320fcbef9c8818c7a825b3f7c2
Daniel Marin <daniel@DESARROLLO-05.sonartelematics.local>
2020-02-24 10:30:20 -0500

    Add navigation property

--- Models/VehiclePart.cs
+++ Models/VehiclePart.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
+
 namespace OtroEF.Models
 {
     public class VehiclePart
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public List<PartReview> PartReviews { get; set; }
     }
 }
