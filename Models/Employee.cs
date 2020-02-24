46cabf013f0f81ba31fe82ec15f3c9348236ed89
Daniel Marin <daniel@DESARROLLO-05.sonartelematics.local>
2020-02-24 10:14:04 -0500

    Add navigation property

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,8 +1,13 @@
+using System.Collections.Generic;
+
 namespace OtroEF.Models
 {
     public class Employee
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public List<VehicleReview> VehicleReviews { get; set; }
+        public List<PartReview> PartReviews { get; set; }
     }
 }
